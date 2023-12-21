b9e12a86dccea1b3a69dbd86c5e961f2fc608662
Quang Cong <143630995+BanhmiqC140820@users.noreply.github.com>
2023-12-21 11:18:00 +0700

    ini

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -57,14 +57,10 @@ class ProductController extends Controller
      */
     public function show( $id)
     {
-<<<<<<< Updated upstream
-        //
-        $product = Product::find($id);
-        return view('product.show',compact('product'));
-=======
+
         $product=Product::find($id);
         return view('product.show')->with('product',$product);
->>>>>>> Stashed changes
+
     }
 
     /**
