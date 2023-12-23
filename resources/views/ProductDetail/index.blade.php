80386e9ea4500868e84b8f0198b81dffdff1fb31
Quang Cong <143630995+BanhmiqC140820@users.noreply.github.com>
2023-12-23 18:46:46 +0700

    a

--- resources/views/ProductDetail/index.blade.php
+++ resources/views/ProductDetail/index.blade.php
@@ -103,7 +103,7 @@
   <div class="container my-3">
     <div class="row">
         <div class="col-4">
-            <img src="{{$products1->img}}" alt="">
+            <img src="{{asset('images/'.$products1->img)}}" alt="">
         </div>
         <div class="col-8"style="padding-top:80px">
             <div>
